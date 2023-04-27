7da785634b43b319c508967b628abfd1bafd5371
AlexShtal <aleks2015.773@gmail.com>
2023-04-27 21:56:29 +0600

    Сделал нормальный парсинг

--- Lab_2/Parser.py
+++ Lab_2/Parser.py
@@ -1,94 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
 
 
-def save_to_csv(data):
-    with open("drugs.csv", mode="a+", newline='', encoding='utf-8') as w_file:
-        names = ["Категория", "Название", "Полное название", "Ссылка"]
-        file_writer = csv.DictWriter(w_file, delimiter=",",
-                                     lineterminator="\n", fieldnames=names)
-        for item in data:
-            file_writer.writerow(item)
+def parse(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'}
+    page = requests.get(url, headers=headers)
+
+    data = []
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    drugs = soup.find("div", class_="view-content").find_all("div", class_="col-md-6 views-row")
+
+    for drug in drugs:
+        name = drug.find("div", class_="h2").find("a").text.strip()
+        ref = "https://аптека-омск.рф" + drug.find("div", class_="h2").find("a").get("href")
+        price = drug.find("div", class_="price-new").text.split(" ")[2].strip()
+
+        quantity = drug.find("span", class_="colich").text[6::].strip()
+        if quantity == "":
+            quantity = "Не указано"
+
+        form = drug.find("span", class_="forms").text.strip()
+        if form == "":
+            form = "Не указано"
+
+        data.append({"Название": name,
+                     "Ссылка": ref,
+                     "Цена": price,
+                     "Количество": quantity,
+                     "Форма": form})
+
+    return data
 
 
-def parse():
-    urls = {
-        "Антисептики и дезинфекторы": "https://аптека-омск.рф/catalog/antiseptiki-i-dezinficiruyuschie-sredstva",
-        "Глазные мази, гели и капли": "https://аптека-омск.рф/catalog/glaznye-mazi-geli-i-kapli",
-        "Противопростудные комплексы": "https://аптека-омск.рф/catalog/protivoprostudnye-kompleksy",
-        "Лечение некотиновой зависимости": "https://аптека-омск.рф/catalog/lechenie-nikotinovoy-zavisimosti",
-        "Нервная система": "https://аптека-омск.рф/catalog/nervnaya-sistema",
-        "От кашля": "https://аптека-омск.рф/catalog/preparaty-ot-kashlya",
-        "От боли в горле": "https://аптека-омск.рф/catalog/sredstva-ot-boli-v-gorle",
-        "Противовирусные": "https://аптека-омск.рф/catalog/protivovirusnye-preparaty",
-        "От насморка": "https://аптека-омск.рф/catalog/sredstva-ot-nasmorka",
-        "Дерматология": "https://аптека-омск.рф/catalog/dermatologiya",
-        "Гомеопатические": "https://аптека-омск.рф/catalog/gomeopaticheskie-sredstva",
-        "Контрацептивы": "https://аптека-омск.рф/catalog/kontraceptivnoe-sredstvo-dlya-mestnogo-primeneniya",
-        "Лечение сахарного диабета": "https://аптека-омск.рф/catalog/preparaty-dlya-lecheniya-sakharnogo-diabeta",
-        "Противогеморроидальные": "https://аптека-омск.рф/catalog/protivogemorroidalnye-preparaty",
-        "Противогрибковые": "https://аптека-омск.рф/catalog/protivogribkovye-sredstva",
-        "Иммуномодулирующее и иммуностимулирующее действие": "https://аптека-омск.рф/catalog/immunomoduliruyuschee-i-immunostimuliruyuschee-deystvie",
-        "Против аллергии": "https://аптека-омск.рф/catalog/protivoalergicheskie-sredstva",
-        "Травы, сборы, чаи": "https://аптека-омск.рф/catalog/travy-sbory-chai-0",
-        "Ушные капли": "https://аптека-омск.рф/catalog/ushnye-kapli",
-        "Витамины": "https://аптека-омск.рф/catalog/vitaminy",
-        "Мочеполовая система и половые гормоны": "https://аптека-омск.рф/catalog/mochepolovaya-sistema-i-polovye-gormony",
-        "Кроветворение и кровь": "https://аптека-омск.рф/catalog/krovetvorenie-i-krov",
-        "Костно-мышечная система": "https://аптека-омск.рф/catalog/kostno-myshechnaya-sistema",
-        "Дыхательная система": "https://аптека-омск.рф/catalog/dykhatelnaya-sistema",
-        "Гормоны для системного применения (исключая половые гормоны и инсулин)": "https://аптека-омск.рф/catalog/gormony-dlya-sistemnogo-primeneniya-isklyuchaya-polovye-gormony-i-insulin",
-        "Пищеварительный тракт и обмен веществ": "https://аптека-омск.рф/catalog/pischevaritelnyy-trakt-i-obmen-veschestv",
-        "Противомикробные (системный приём)": "https://аптека-омск.рф/catalog/protivomikrobnye-preparaty-dlya-sistemnogo-primeneniya",
-        "Противоопухолевые и иммуномодуляторы": "https://аптека-омск.рф/catalog/protivoopukholevye-preparaty-i-immunomodulyatory",
-        "Сердечно-сосудистая система": "https://аптека-омск.рф/catalog/serdechno-sosudistaya-sistema",
-        "Противопаразитарные, инсектициды и репелленты": "https://аптека-омск.рф/catalog/protivoparazitarnye-preparaty-insekticidy-i-repellenty",
-        "Прочие": "https://аптека-омск.рф/catalog/prochie-preparaty"
-    }
-
-    # Перебор ссылок
-    for category, url in urls.items():
-        data = []
-
-        headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 '
-                                 '(KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36'}
-        page = requests.get(url, headers=headers)
-        print(page.status_code)
-
-        soup = BeautifulSoup(page.text, "html.parser")
-
-        soup_drugs = soup.find_all("div", "col-md-3 views-row")
-
-        # Перебор лекарств
-        for soup_drug in soup_drugs:
-            info = dict()
-
-            full_name = soup_drug.find("div", class_="h2").find("a").text.strip()
-
-            ref = soup_drug.find("div", class_="h2").find("a", href=True)["href"]
-
-            url = "https://аптека-омск.рф" + ref
-            page = requests.get(url, headers=headers)
-            html = BeautifulSoup(page.text, "html.parser")
-            name = html.find("div",
-                             class_="example1 clearfix text-formatted field field--name-field-pokazaniya field--type-text-long "
-                                    "field--label-above").find("div", class_="field__label").text.strip()[25::]
-
-            info["Категория"] = category
-            info["Название"] = name
-            info["Полное название"] = full_name
-            info["Ссылка"] = url
-
-            data.append(info)
-
-            print(data)
-            print(info["Категория"])
-            print(info["Название"])
-            print(info["Полное название"])
-            print(info["Ссылка"], end="\n\n")
-
-        save_to_csv(data)
-
-
-parse()
+if __name__ == "__main__":
+    parse()
--- Lab_2/main.py
+++ Lab_2/main.py
@@ -1,50 +1,29 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, \
-    KeyboardButton
-from aiogram.dispatcher.filters import Text
-import csv
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Parser import parse
 
 
-def read_csv():
-    try:
-        with open("drugs.csv", encoding='utf-8') as r_file:
+def sort_drugs(drugs):
+    sorted_drugs = drugs.copy()
 
-            # Создаем объект DictReader, указываем символ-разделитель ","
-            file_reader = csv.DictReader(r_file, delimiter=",")
+    for i in range(len(sorted_drugs) - 1):
+        for j in range(len(sorted_drugs) - i - 1):
+            if float(sorted_drugs[j]["Цена"]) > float(sorted_drugs[j + 1]["Цена"]):
+                sorted_drugs[j], sorted_drugs[j + 1] = sorted_drugs[j + 1], sorted_drugs[j]
+    return sorted_drugs
 
-            # Счетчик для подсчета количества строк и вывода заголовков столбцов
-            count = 0
 
-            # Считывание данных из CSV файла
-
-            data = []
-            for row in file_reader:
-                info = {}
-                if count == 0:
-                    # Вывод строки, содержащей заголовки для столбцов
-                    headers = row
-                    count += 1
-                else:
-                    # Вывод строк
-
-                    info["Категория"] = row["Категория"]
-                    info["Название"] = row["Название"]
-                    info["Полное название"] = row["Полное название"]
-                    info["Ссылка"] = row["Ссылка"]
-
-                    data.append(info)
-    except FileNotFoundError as ex:
-        print(ex)
-    return data
+def get_drugs(drug_name):
+    url = "https://аптека-омск.рф/search?q=" + drug_name
+    found_drugs = parse(url)
+    return sort_drugs(found_drugs)
 
 
 TOKEN_API = "6076130719:AAH5iEs1nRCyflCpk9Y7iMg5hj_zw_MK8AQ"
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
-
-drugs = read_csv()
-found_drugs = []
+drugs = []
 current_drug = 0
 
 
@@ -54,64 +33,80 @@ async def on_startup(_):
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
+    global drugs
+    global current_drug
+    drugs = []
+    current_drug = 0
+
     await message.answer("Привет, я бот - поисковик лекарств. Введите название лекарства для поиска.")
 
 
 async def send_drug_info(index, id):
-    # Извлечение информации о лекарстве из переданного словаря
-    category = found_drugs[index].get('Категория')
-    drug_name = found_drugs[index].get('Название')
-    full_name = found_drugs[index].get('Полное название')
-    link = found_drugs[index].get('Ссылка')
+    global drugs
+    # Извлечение информации о лекарстве из переданного списка
+    if len(drugs) == 0:
+        await bot.send_message(chat_id=id, text="Не удалось найти лекарство(")
+        return
+    else:
+        drug_data = drugs[index]
 
-    # Формирование текстового сообщения
-    info_text = f"Категория: {category}\nНазвание: {drug_name}\nПолное название: {full_name}"
+        name = drug_data["Название"]
+        price = drug_data["Цена"]
+        quantity = drug_data["Количество"]
+        form = drug_data["Форма"]
+        link = drug_data["Ссылка"]
 
-    # Создание inline-клавиатуры с кнопками навигации
-    ikb = InlineKeyboardMarkup(row_width=3)
-    prev_button = InlineKeyboardButton('<<', callback_data='prev')
-    next_button = InlineKeyboardButton('>>', callback_data='next')
-    back_button = InlineKeyboardButton('В меню', callback_data='back')
-    url_button = InlineKeyboardButton('Подробнее', url=link)
-    ikb.add(prev_button, next_button, url_button).add(back_button)
+        # Формирование текстового сообщения
+        info_text = f"Название: {name}\nЦена: {price} руб.\nФорма: {form}\nКоличество: {quantity}"
 
-    # Отправка сообщения пользователю с информацией о лекарстве и inline-клавиатурой
-    await bot.send_message(chat_id=id, text=info_text, reply_markup=ikb)
+        # Создание inline-клавиатуры с кнопками навигации
+        ikb = InlineKeyboardMarkup(row_width=3)
+        prev_button = InlineKeyboardButton('<<', callback_data='prev')
+        next_button = InlineKeyboardButton('>>', callback_data='next')
+        back_button = InlineKeyboardButton('В меню', callback_data='back')
+        url_button = InlineKeyboardButton('Подробнее', url=link)
+        ikb.add(prev_button, next_button, url_button).add(back_button)
+
+        # Отправка сообщения пользователю с информацией о лекарстве и inline-клавиатурой
+        await bot.send_message(chat_id=id, text=info_text, reply_markup=ikb)
 
 
 @dp.callback_query_handler()
 async def navigate(callback: types.CallbackQuery):
+    global drugs
     global current_drug
 
     if callback.data == "prev" and current_drug > 0:
         current_drug -= 1
         await send_drug_info(current_drug, callback.from_user.id)
         await callback.message.delete()
-    elif callback.data == "next" and current_drug < len(found_drugs) - 1:
+    elif callback.data == "next" and current_drug < len(drugs) - 1:
         current_drug += 1
         await send_drug_info(current_drug, callback.from_user.id)
         await callback.message.delete()
     elif callback.data == "back":
+        drugs = []
+        current_drug = 0
         await bot.send_message(chat_id=callback.from_user.id, text="Введите название для поиска.")
         await callback.message.delete()
 
 
-
 @dp.message_handler()
 async def find_by_name(message: types.Message):
-    drug_name = message.text.title()
-    found_drugs.clear()
-    current_drug = 0
-    found = False
+    global drugs
+    global current_drug
 
-    for drug in drugs:
-        if drug["Название"] == drug_name:
-            found_drugs.append(drug)
-            found = True
-    if found:
+    if len(drugs) == 0:
+        drug_name = message.text.title()
+
+        drugs = get_drugs(drug_name)
+
+        current_drug = 0
+
         await send_drug_info(current_drug, message.from_user.id)
+
     else:
-        await message.answer("Не удалось найти(")
+        await bot.send_message(chat_id=message.from_user.id, text="Сначала вернитесь в меню или отправьте /start!")
 
 
 if __name__ == "__main__":
